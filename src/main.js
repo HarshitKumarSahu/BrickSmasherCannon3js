102ab2741ed5ab64f1487f0651c67ba0e9956c83
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-11-18 21:08:08 +0530

    added car

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@ controls.enableDamping = true
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -70,7 +70,7 @@ const trees = new THREE.Group()
 const rocks = new THREE.Group()
 const car = new THREE.Group()
 
-scene.add(arena, trees, rocks)
+scene.add(arena, trees, rocks, car)
 
 /**
  * Arena
@@ -106,14 +106,12 @@ const wall1 =new THREE.Mesh(
     new THREE.MeshLambertMaterial({ color: arenaDimension.walls.color })
 )
 wall1.position.set(0, 1.5, -25)
-arena.add(floor, wall1)
 
 const wall2 =new THREE.Mesh(
     new THREE.BoxGeometry(arenaDimension.walls.width, arenaDimension.walls.height, arenaDimension.walls.depth ),
     new THREE.MeshLambertMaterial({ color: arenaDimension.walls.color })
 )
 wall2.position.set(0, 1.5, 25)
-arena.add(floor, wall1)
 
 const wall3 =new THREE.Mesh(
     new THREE.BoxGeometry(arenaDimension.walls.width, arenaDimension.walls.height, arenaDimension.walls.depth ),
@@ -121,7 +119,6 @@ const wall3 =new THREE.Mesh(
 )
 wall3.position.set(25, 1.5, 0)
 wall3.rotation.y = Math.PI * 0.5
-arena.add(floor, wall1)
 
 const wall4 =new THREE.Mesh(
     new THREE.BoxGeometry(arenaDimension.walls.width, arenaDimension.walls.height, arenaDimension.walls.depth ),
@@ -130,7 +127,13 @@ const wall4 =new THREE.Mesh(
 wall4.position.set(-25, 1.5, 0)
 wall4.rotation.y = Math.PI * 0.5
 
-arena.add(floor, wall1, wall2, wall3, wall4)
+arena.add(
+    floor, 
+    wall1, 
+    wall2, 
+    wall3, 
+    wall4
+)
 
 
 /**
@@ -294,9 +297,303 @@ const GenerateRocks = (geometer, material, n) => {
     }
 }
 
-// GenerateTrees(CreateTreeType1Template, trees, treeDimension.count);
-// GenerateTrees(CreateTreeType2Template, trees, treeDimension.count);
-// GenerateRocks(rockGeo, rockMat, rockDimension.count)
+GenerateTrees(CreateTreeType1Template, trees, treeDimension.count);
+GenerateTrees(CreateTreeType2Template, trees, treeDimension.count);
+GenerateRocks(rockGeo, rockMat, rockDimension.count)
+
+
+
+
+
+
+
+
+
+
+/**
+ * Car
+ */
+const carDimension = {
+    mainBody: {
+        width: 1.5,
+        height: 0.25,
+        depth: 0.75,
+        color: "#FF4444"
+    },
+    roofBody: {
+        width: 1,
+        height: 0.35,
+        depth: 0.7,
+        color: "#CC3333"
+    },
+    roofBodyGlass : {
+        LROne : {
+            width: 0.45,
+            height: 0.3,
+            depth: 0.725,
+        },
+        LRTwo : {
+            width: 0.45,
+            height: 0.3,
+            depth: 0.725,
+        },
+        TB : {
+            width: 1.025,
+            height: 0.3,
+            depth: 0.65,
+        },
+        color: "#d4f4ff"
+    },
+    doorHandle: {
+        width: 0.075,
+        height: 0.025,
+        depth: 0.775,
+        color: "#000000"
+    },
+    mirror: {
+        width: 0.025,
+        height: 0.0575,
+        depth: 0.0775,
+        color: "#000000"
+    },
+    headLight: {
+        radius: 0.05,
+        height: 1.475,
+        capSeg: 6,
+        radSeg: 6,
+        color: "#ffd886"
+    },
+    bumper: {
+        width: 1.525,
+        height: 0.025,
+        depth: 0.7525,
+        color: "#000000"
+    },
+    wheel: {
+        tyre: {
+            radius: 0.15, 
+            height: 0.0725, 
+            radSeg: 10,
+            color: "#3f4a3c"
+        },
+        rims: {
+            radius: 0.075, 
+            tube: 0.015, 
+            tubeSeg: 128, 
+            radSeg: 16, 
+            p: 8, 
+            q: 9,
+            color: "#a2af9f"
+        }
+    }
+}
+
+//Main Body
+const mainBody = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.mainBody.width, carDimension.mainBody.height, carDimension.mainBody.depth),
+    // new THREE.MeshLambertMaterial({ color: 0xFF4444})
+    new THREE.MeshLambertMaterial({ color: carDimension.mainBody.color})
+)
+
+// roof
+const roof = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.roofBody.width, carDimension.roofBody.height, carDimension.roofBody.depth),
+    // new THREE.MeshLambertMaterial({ color: 0xCC3333})
+    new THREE.MeshLambertMaterial({ color: carDimension.roofBody.color})
+)
+roof.position.set(carDimension.mainBody.width * 0.1125, (carDimension.mainBody.height * 0.5) + (carDimension.roofBody.height * 0.5), 0)
+
+// roof Glass left-right One
+const roofGlassLROne = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.roofBodyGlass.LROne.width, carDimension.roofBodyGlass.LROne.height, carDimension.roofBodyGlass.LROne.depth),
+    // new THREE.MeshLambertMaterial({ color: "#f8f8f8"})
+    new THREE.MeshLambertMaterial({ color: carDimension.roofBodyGlass.color})
+)
+roofGlassLROne.position.set(-carDimension.mainBody.width * 0.05, (carDimension.mainBody.height * 0.5) + (carDimension.roofBody.height * 0.5), 0)
+
+// roof Glass left-right Two
+const roofGlassLRTwo = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.roofBodyGlass.LRTwo.width, carDimension.roofBodyGlass.LRTwo.height, carDimension.roofBodyGlass.LRTwo.depth),
+    // new THREE.MeshLambertMaterial({ color: "#f8f8f8"})
+    new THREE.MeshLambertMaterial({ color: carDimension.roofBodyGlass.color})
+)
+roofGlassLRTwo.position.set(carDimension.mainBody.width * 0.275, (carDimension.mainBody.height * 0.5) + (carDimension.roofBody.height * 0.5), 0)
+
+// roof Glass top-bottom
+const roofGlassTB = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.roofBodyGlass.TB.width, carDimension.roofBodyGlass.TB.height, carDimension.roofBodyGlass.TB.depth),
+    // new THREE.MeshLambertMaterial({ color: "#f8f8f8"})
+    new THREE.MeshLambertMaterial({ color: carDimension.roofBodyGlass.color})
+)
+roofGlassTB.position.set(carDimension.mainBody.width * 0.1125, (carDimension.mainBody.height * 0.5) + (carDimension.roofBody.height * 0.5), 0)
+
+// Door Handle
+const doorHandel = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.doorHandle.width, carDimension.doorHandle.height, carDimension.doorHandle.depth),
+    // new THREE.MeshLambertMaterial({ color: "#000"})
+    new THREE.MeshLambertMaterial({ color: carDimension.doorHandle.color})
+)
+doorHandel.position.set(carDimension.mainBody.width * 0.1125, carDimension.mainBody.height * 0.25, 0)
+
+// Left Mirror
+const leftMirror = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.mirror.width, carDimension.mirror.height, carDimension.mirror.depth),
+    // new THREE.MeshLambertMaterial({ color: "#000"})
+    new THREE.MeshLambertMaterial({ color: carDimension.mirror.color})
+)
+leftMirror.position.set(- carDimension.mainBody.width * 0.2 , carDimension.mainBody.height * 0.6, carDimension.mainBody.depth * 0.5425)
+
+// Right Mirror
+const rightMirror = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.mirror.width, carDimension.mirror.height, carDimension.mirror.depth),
+    // new THREE.MeshLambertMaterial({ color: "#000"})
+    new THREE.MeshLambertMaterial({ color: carDimension.mirror.color})
+)
+rightMirror.position.set(- carDimension.mainBody.width * 0.2 , carDimension.mainBody.height * 0.6, - carDimension.mainBody.depth * 0.5425)
+
+// Left Head Light
+const leftHeadLight = new THREE.Mesh(
+    new THREE.CapsuleGeometry( carDimension.headLight.radius, carDimension.headLight.height, carDimension.headLight.capSeg, carDimension.headLight.radSeg, 1 ),
+    // new THREE.MeshLambertMaterial({ color: "#ffd886"})
+    new THREE.MeshLambertMaterial({ color: carDimension.headLight.color})
+)
+leftHeadLight.rotation.z = Math.PI * 0.5
+leftHeadLight.position.set(0, 0, carDimension.mainBody.depth * 0.35)
+
+// Right Head Light
+const rightHeadLight = new THREE.Mesh(
+    new THREE.CapsuleGeometry( carDimension.headLight.radius, carDimension.headLight.height, carDimension.headLight.capSeg, carDimension.headLight.radSeg, 1 ),
+    // new THREE.MeshLambertMaterial({ color: "#ffd886"})
+    new THREE.MeshLambertMaterial({ color: carDimension.headLight.color})
+)
+rightHeadLight.rotation.z = Math.PI * 0.5
+rightHeadLight.position.set(0, 0, -carDimension.mainBody.depth * 0.35)
+
+// bumper
+const bumper = new THREE.Mesh(
+    new THREE.BoxGeometry(carDimension.bumper.width, carDimension.bumper.height, carDimension.bumper.depth),
+    // new THREE.MeshLambertMaterial({ color: "#000"})
+    new THREE.MeshLambertMaterial({ color: carDimension.bumper.color})
+)
+bumper.position.set(0, - carDimension.mainBody.height * 0.5, 0)
+
+// wheel = tye + rims
+const wheelOne = new THREE.Group()
+const wheelTwo = new THREE.Group()
+const wheelThree = new THREE.Group()
+const wheelFour = new THREE.Group()
+
+const tyreOne = new THREE.Mesh( 
+    new THREE.CylinderGeometry( carDimension.wheel.tyre.radius, carDimension.wheel.tyre.radius, carDimension.wheel.tyre.height, carDimension.wheel.tyre.radSeg), 
+    // new THREE.MeshLambertMaterial( { color: "#3f4a3c" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.tyre.color } )
+);
+const rimsOne = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( carDimension.wheel.rims.radius, carDimension.wheel.rims.tube, carDimension.wheel.rims.tubeSeg, carDimension.wheel.rims.radSeg, carDimension.wheel.rims.p, carDimension.wheel.rims.q),
+    // new THREE.MeshLambertMaterial( { color: "#96afb8" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.rims.color } )
+)
+rimsOne.rotation.x = Math.PI * 0.5
+wheelOne.rotation.x = Math.PI * 0.5
+wheelOne.position.set(carDimension.mainBody.width * 0.30, -carDimension.mainBody.height * 0.5, carDimension.mainBody.depth * 0.55)
+wheelOne.add(tyreOne, rimsOne)
+
+const tyreTwo = new THREE.Mesh( 
+    new THREE.CylinderGeometry( carDimension.wheel.tyre.radius, carDimension.wheel.tyre.radius, carDimension.wheel.tyre.height, carDimension.wheel.tyre.radSeg), 
+    // new THREE.MeshLambertMaterial( { color: "#3f4a3c" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.tyre.color } )
+);
+const rimsTwo = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( carDimension.wheel.rims.radius, carDimension.wheel.rims.tube, carDimension.wheel.rims.tubeSeg, carDimension.wheel.rims.radSeg, carDimension.wheel.rims.p, carDimension.wheel.rims.q),
+    // new THREE.MeshLambertMaterial( { color: "#96afb8" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.rims.color } )
+)
+rimsTwo.rotation.x = Math.PI * 0.5
+wheelTwo.rotation.x = Math.PI * 0.5
+wheelTwo.position.set(carDimension.mainBody.width * 0.30, -carDimension.mainBody.height * 0.5, -carDimension.mainBody.depth * 0.55)
+wheelTwo.add(tyreTwo, rimsTwo)
+
+const tyreThree = new THREE.Mesh( 
+    new THREE.CylinderGeometry( carDimension.wheel.tyre.radius, carDimension.wheel.tyre.radius, carDimension.wheel.tyre.height, carDimension.wheel.tyre.radSeg), 
+    // new THREE.MeshLambertMaterial( { color: "#3f4a3c" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.tyre.color } )
+);
+const rimsThree = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( carDimension.wheel.rims.radius, carDimension.wheel.rims.tube, carDimension.wheel.rims.tubeSeg, carDimension.wheel.rims.radSeg, carDimension.wheel.rims.p, carDimension.wheel.rims.q),
+    // new THREE.MeshLambertMaterial( { color: "#96afb8" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.rims.color } )
+)
+rimsThree.rotation.x = Math.PI * 0.5
+wheelThree.rotation.x = Math.PI * 0.5
+wheelThree.position.set( -carDimension.mainBody.width * 0.30, -carDimension.mainBody.height * 0.5, carDimension.mainBody.depth * 0.55)
+wheelThree.add(tyreThree, rimsThree)
+
+const tyreFour = new THREE.Mesh( 
+    new THREE.CylinderGeometry( carDimension.wheel.tyre.radius, carDimension.wheel.tyre.radius, carDimension.wheel.tyre.height, carDimension.wheel.tyre.radSeg), 
+    // new THREE.MeshLambertMaterial( { color: "#3f4a3c" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.tyre.color } )
+);
+const rimsFour = new THREE.Mesh(
+    new THREE.TorusKnotGeometry( carDimension.wheel.rims.radius, carDimension.wheel.rims.tube, carDimension.wheel.rims.tubeSeg, carDimension.wheel.rims.radSeg, carDimension.wheel.rims.p, carDimension.wheel.rims.q),
+    // new THREE.MeshLambertMaterial( { color: "#96afb8" } )
+    new THREE.MeshLambertMaterial( { color: carDimension.wheel.rims.color } )
+)
+rimsFour.rotation.x = Math.PI * 0.5
+wheelFour.rotation.x = Math.PI * 0.5
+wheelFour.position.set( -carDimension.mainBody.width * 0.30, -carDimension.mainBody.height * 0.5, -carDimension.mainBody.depth * 0.55)
+wheelFour.add(tyreFour, rimsFour)
+
+car.add(
+    mainBody, 
+    roof, 
+    roofGlassLROne, 
+    roofGlassLRTwo, 
+    roofGlassTB, 
+    doorHandel, 
+    leftMirror, 
+    rightMirror, 
+    leftHeadLight, 
+    rightHeadLight, 
+    bumper, 
+    wheelOne, 
+    wheelTwo, 
+    wheelThree, 
+    wheelFour
+)
+car.position.set(0, (carDimension.mainBody.height * 0.5) + (carDimension.wheel.tyre.radius), 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * Lights
